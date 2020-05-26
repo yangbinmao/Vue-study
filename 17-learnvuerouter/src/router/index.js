6728fab9b32a31adcd1ec057f43732f4f355f18c
27875 <yangbingmao888>
2020-05-26 23:37:50 +0800

    Vue-CLI  router

--- 17-learnvuerouter/src/router/index.js
+++ 17-learnvuerouter/src/router/index.js
@@ -1,15 +1,14 @@
+//配置路由相关的信息
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
+
+//1.通过Vue.ues(插件)，安装插件
 Vue.use(Router)
-
+//2.创建VueRouter对象
 export default new Router({
+  //配置路由和组件之间的映射关系
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+   
   ]
 })
