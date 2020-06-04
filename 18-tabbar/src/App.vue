9ff35f39e514db0cc0720483ceaa64ad52dc318e
27875 <yangbingmao888>
2020-06-04 21:56:04 +0800

    Vue-CLI 18 tabbar 导航栏的路由

--- 18-tabbar/src/App.vue
+++ 18-tabbar/src/App.vue
@@ -1,38 +1,44 @@
 <template>
   <div id="app">
-    <div id="tab-bar">
-      <div class="tab-bar-item">首页</div>
-      <div class="tab-bar-item">分类</div>
-      <div class="tab-bar-item">购物车</div>
-      <div class="tab-bar-item">我的</div>
-    </div>
+    <tab-bar> 
+      <tab-bar-item>
+        <img  slot="item-icon" src="./assets/img/tabbar/logo.png" alt="">
+        <img slot="item-icon-active" src="./assets/img/tabbar/hk.jpg" alt="">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img  slot="item-icon" src="./assets/img/tabbar/logo.png" alt="">
+         <img slot="item-icon-active" src="./assets/img/tabbar/hk.jpg" alt="">
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item>        
+        <img  slot="item-icon" src="./assets/img/tabbar/logo.png" alt="">
+         <img slot="item-icon-active" src="./assets/img/tabbar/hk.jpg" alt="">
+        <div slot="item-text">购物车</div>
+        </tab-bar-item>
+      <tab-bar-item>
+        <img  slot="item-icon" src="./assets/img/tabbar/logo.png" alt="">
+         <img slot="item-icon-active" src="./assets/img/tabbar/hk.jpg" alt="">
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
   </div>
 </template>
 
 <script>
+  import TabBar from './components/tabbar/Tabbar.vue';
+  import TabBarItem from './components/tabbar/TabBarItem.vue';
+
   export default {
-    name: 'App'
+    name: 'App',
+    components: {
+      TabBar,
+      TabBarItem
+    }
   }
 </script>
 
 <style>
   @import url('./assets/css/base.css');
-  #tab-bar{
-    display: flex;  /*设置 布局模式为弹性布局*/
-    background-color: #f6f6f6;  /*设置导航栏背景色 */
-
-    /*设置导航栏为最下边 */
-    position: fixed;
-    left: 0;
-    right: 0;
-    bottom: 0;
-
-    box-shadow: 10px 10px 10px rgba(red);
-  }
-  .tab-bar-item{
-    flex: 1;/*使标签均等分 */
-    text-align: center; /*文字水平居中 */
-    height: 49px;/*一般手机导航栏的高度都是49px*/ 
-  
-  }
+ 
 </style>
--- 18-tabbar/src/router/index.js
+++ 18-tabbar/src/router/index.js
@@ -5,11 +5,30 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-export default new Router({
+const router= new Router({
   routes: [
 
   ]
 })
+
+
+
+
+
+export default router;
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //在main引用css,但是一般不建议在这个位置引用，而是在对应组件内的style标签内用@import(url)来引用css
