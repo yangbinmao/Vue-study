b00cf7be26dba0825dde05354606f64ed4df9ea3
ybm <278754506@qq.com>
2020-06-11 17:06:59 +0800

    学习VUEX的各种属性。

--- 20-learnvuex/src/App.vue
+++ 20-learnvuex/src/App.vue
@@ -1,18 +1,26 @@
 <template>
   <div id="app">
-    <h2>-------------app内容-------------------</h2>
+    <h1>-------------app内容-------------------</h1>
     <h2>app内的message:{{message}}</h2>
     <h2>app内的couter:{{counter}}</h2>
+
+
     <h2>----------VueX的state属性------------</h2>
     <h2>全局的counter:{{$store.state.counter}}</h2>
+
+
    <h2>----------VueX的mutations属性：修改全局变量的值的方法--------</h2>
     <h3>VueX的mutations属性不带参数</h3>
-     <button @click="addClick">+1</button>
-     <button @click="subClick">-1</button>
+     <button @click="addClick">使全局变量counter+1</button>
+     <button @click="subClick">使全局变量counter-1</button>
+     <h2>全局的info:{{$store.state.info}}</h2>
+     <button @click="addInfo">给全局变量info动态添加学生</button>
      <h3>VueX的mutations属性带参数</h3>
-     <button @click="addCountClick(5)">+5</button>
-     <button @click="addCountClick(10)">+10</button>
-     <button @click="addstu">添加学生</button>
+     <button @click="addCountClick(5)">使全局变量counter+5</button>
+     <button @click="addCountClick(10)">使全局变量counter+10方式一</button>
+     <button @click="addCountClick2(10)">使全局变量counter+10方式二</button>
+     <button @click="addstu">使全局变量students添加学生</button>
+     <button @click="deleteIonfoSome('age')">删除全局变量info的age属性</button>
     <!-- 使用vuex -->
     <h2>------------Vuex的getters属性：获取经过处理后的全局变量参数值，但是不改变本身值的大小---------------</h2>
     <!-- 需要对全局属性（vuex里的store）内的值进行操作但不改变本身值并且输出，使用它的getters方法（类似vue的计算属性（computed）） -->
@@ -26,7 +34,7 @@
     <!-- getters筛选带参数的情况 -->
     <h3>getters筛选带参数的情况 </h3>
     <h2>{{$store.getters.moreAgeStu(19)}}</h2>
-    <h2>-------------hello Vuex内容-------------------</h2>
+    <h1>-------------hello Vuex内容-------------------</h1>
     <hello-vuex></hello-vuex>
   </div>
 </template>
@@ -51,16 +59,37 @@ export default {
     addClick(){
       //需要对全局属性（vuex里的store）进行进行操作或者改变转状态
       //使用this.$store.commit('方法名')
+      
       this.$store.commit('increment')
     },
     subClick() {
        this.$store.commit('decrement')
     },
     addCountClick(count){
+      //vuex:mutations(修改全局变量带参数)提交方式一，普通提交风格(参数就是参数本身)
       this.$store.commit('incrementCount',count)
+ 
+    },
+    addCountClick2(count){
+     //vuex:mutations(修改全局变量带参数)提交方式二，对象形式提交风格风格.
+      //注意变化形式后，传过去的值实际是整个commit对象。意思就是在对应的incrementCount方法里要用是形参.count.
+       this.$store.commit({
+        type:'incrementCount2',
+        count
+       })
     },
     addstu(){
-      this.$store.commit('addStudent',{id:114,name:'ybm4',age:22},)
+      //给全局变量添加学生
+      this.$store.commit('addStudent',{id:114,name:'ybm4',age:22})
+      
+    },
+    addInfo(){
+      //给全局变量info动态条件属性
+      this.$store.commit('addInfo')
+    },
+    deleteIonfoSome(attr){
+      //删除全局变量的某个属性
+      this.$store.commit('deleteIonfoSome',attr)
     }
   }
 
--- 20-learnvuex/src/store/index.js
+++ 20-learnvuex/src/store/index.js
@@ -18,13 +18,15 @@ const store=new Vuex.Store({
       {id:111,name:'ybm1',age:19},
       {id:112,name:'ybm2',age:20},
       {id:113,name:'ybm3',age:21},
-    ]
+    ],
+    info:{id:110,name:'ybm',age:18}
     
   },
   mutations: {  
     //用前须知
     //在这里面创建的方法参数可以只是state(其实都是会带上state的，不然怎么改变值)
     //也可以继续添加参数，后边的参数就等于你需要对变量值操作的变化值（就是的传过来的参数）
+    //注意：以下所有方法都追寻响应式布局，意思就是都是全动态关联的。
 
     //参数被称为是mutation的载荷（Payload）,可以是一个值，也可以是一个对象
 
@@ -36,13 +38,38 @@ const store=new Vuex.Store({
     decrement(state){
       state.counter--
     },
+    
+    //mutations带参数修改全局变量值
+    //1.直接传参数
     //通过mutations带参的更新的方式:在任何组件内调用$store.commit.('方法名'，参数值)，就可以调用mutations里面的方法。
     incrementCount(state,count){ //count就等于是传过来的变量
+      //如果组件使用$store.commit.('方法名'，参数值)方式。组件的参数值就是这里的count值。
       state.counter+=count
+      
+    },
+    //1.传参数以对象形式
+    incrementCount2(state,payload){
+      //如果组件使用$store.commit.({type:'方法名',参数值})方式。组件的参数值实际是整个commit对象。
+      //所以传过来的对象被形参count获取。实际值就是count(形参对象).count(真实值)
+      //故如果是对象形式建议mutations内对应方法形式为incrementCount(state,payload){具体方法}，因为payload(载荷)只是一个形参，使用的人就知道这个是一个对象方法。
+      state.counter+=payload.count
     },
     addStudent(state,stu){//count就等于是传过来的对象
       //如果是对象添加，数组的添加方法是push.
       state.students.push(stu)
+    },
+
+    //mutations带参数给全局变量添加其他属性，Ps:给一个对象{id:113,name:'ybm3',age:21},，添加一个height属性
+    //方式一：使用vue.set(obj,'newProp',169)    
+    addInfo(state){
+      Vue.set(state.info,'address','洛杉矶')
+    },
+    //方式二：用新对象给旧对象重新赋值
+
+
+    //mutations给全局变量删除某个属性,Ps:给一个对象{id:113,name:'ybm3',age:21},，删除一个age属性
+    deleteIonfoSome(state,arrt){
+      Vue.delete(state.info,arrt)
     }
   },
   actions: {
@@ -64,6 +91,9 @@ const store=new Vuex.Store({
     more20stu(state){
       return state.students.filter(s => s.age > 20)
     },
+    
+    
+    //getters带参数
 
     //可以直接把getters当做参数传入。但是还是必须写state.说白了，第一个位置屋里是什么，他都代表state,第二个值，代表getters
     more20stuLength(state,getters){ 
